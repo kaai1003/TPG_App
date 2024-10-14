4b751d2466fd9b0264ca6b5bfc281c7095c64813
AIMAD KACEM <kacem.aimad@gmail.com>
2024-10-14 22:54:56 +0100

    upload some data to data table

--- web_dynamic/static/scripts/main.js
+++ web_dynamic/static/scripts/main.js
@@ -20,4 +20,48 @@ $(document).ready(function(){
 			$("#selectAll").prop("checked", false);
 		}
 	});
+  // Upload some data to table
+  $.ajax({
+		url: 'http://localhost:5000/api/v1/users/', // The Flask API endpoint
+		type: 'GET',
+		success: function(data) {
+			// Handle success response
+			const headTable = $('#table-head');
+			headTable.empty();
+			const checkbox = '<th>' +
+													'<span class="custom-checkbox">' +
+														'<input type="checkbox" id="selectAll">' +
+														'<label for="selectAll"></label>' +
+													'</span>' +
+												'</th>';
+			headTable.append(checkbox);
+			const headers = ['Id', 'User', 'Role', 'Actions']
+			headers.forEach(function(header) {
+				const th = '<th>' + header + '</th>';
+				headTable.append(th);
+			});
+			const dataTable = $('#data-table tbody');
+			dataTable.empty();
+			data.forEach(function(item) {
+				dataTable.append('<tr>')
+				const dtCheck = '<td>' +
+												'<span class="custom-checkbox">' +
+													'<input type="checkbox" id="checkbox1" name="options[]" value="1">' +
+													'<label for="checkbox1"></label>' +
+												'</span>' +
+											'</td>';
+				dataTable.append(dtCheck);
+				const td = '<td>' + item.id + '</td>' +
+										'<td>' + item.user + '</td>' +
+										'<td>' + item.role + '</td>';
+				dataTable.append(td);
+				const dtAction = '<td>' +
+														'<a href="#editEmployeeModal" class="edit" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i></a>' +
+														'<a href="#deleteEmployeeModal" class="delete" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></a>' +
+													'</td>';
+				dataTable.append(dtAction);
+				dataTable.append('</tr>');
+			})
+		},
+	});
 });

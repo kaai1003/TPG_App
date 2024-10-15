e805c2eab503fb26b816e0949baf35ee4259134d
AIMAD KACEM <kacem.aimad@gmail.com>
2024-10-15 18:26:37 +0100

    fix html design view

--- web_dynamic/static/scripts/main.js
+++ web_dynamic/static/scripts/main.js
@@ -22,46 +22,54 @@ $(document).ready(function(){
 	});
   // Upload some data to table
   $.ajax({
-		url: 'http://localhost:5000/api/v1/users/', // The Flask API endpoint
-		type: 'GET',
-		success: function(data) {
-			// Handle success response
-			const headTable = $('#table-head');
-			headTable.empty();
-			const checkbox = '<th>' +
-													'<span class="custom-checkbox">' +
-														'<input type="checkbox" id="selectAll">' +
-														'<label for="selectAll"></label>' +
-													'</span>' +
-												'</th>';
-			headTable.append(checkbox);
-			const headers = ['Id', 'User', 'Role', 'Actions']
-			headers.forEach(function(header) {
-				const th = '<th>' + header + '</th>';
-				headTable.append(th);
-			});
-			const dataTable = $('#data-table tbody');
-			dataTable.empty();
-			data.forEach(function(item) {
-				dataTable.append('<tr>')
-				const dtCheck = '<td>' +
-												'<span class="custom-checkbox">' +
-													'<input type="checkbox" id="checkbox1" name="options[]" value="1">' +
-													'<label for="checkbox1"></label>' +
-												'</span>' +
-											'</td>';
-				dataTable.append(dtCheck);
-				const td = '<td>' + item.id + '</td>' +
-										'<td>' + item.user + '</td>' +
-										'<td>' + item.role + '</td>';
-				dataTable.append(td);
-				const dtAction = '<td>' +
-														'<a href="#editEmployeeModal" class="edit" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i></a>' +
-														'<a href="#deleteEmployeeModal" class="delete" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></a>' +
-													'</td>';
-				dataTable.append(dtAction);
-				dataTable.append('</tr>');
-			})
-		},
-	});
+    url: 'http://localhost:5000/api/v1/users/', // The Flask API endpoint
+    type: 'GET',
+    success: function(data) {
+        const headTable = $('#table-head');
+        const dataTable = $('#data-table tbody');
+        
+        // Clear previous table headers and rows
+        headTable.empty();
+        dataTable.empty();
+        
+        // Add table headers
+        const headers = ['Id', 'User', 'Role', 'Actions'];
+        const headerRow = `
+            <th>
+                <span class="custom-checkbox">
+                    <input type="checkbox" id="selectAll">
+                    <label for="selectAll"></label>
+                </span>
+            </th>
+            ${headers.map(header => `<th>${header}</th>`).join('')}
+        `;
+        headTable.append(headerRow);
+
+        // Add table rows
+        data.forEach(function(item, index) {
+            const row = `
+                <tr>
+                    <td>
+                        <span class="custom-checkbox">
+                            <input type="checkbox" id="checkbox${index}" name="options[]" value="${item.id}">
+                            <label for="checkbox${index}"></label>
+                        </span>
+                    </td>
+                    <td>${item.id}</td>
+                    <td>${item.user}</td>
+                    <td>${item.role}</td>
+                    <td>
+                        <a href="#editEmployeeModal" class="edit" data-toggle="modal">
+                            <i class="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i>
+                        </a>
+                        <a href="#deleteEmployeeModal" class="delete" data-toggle="modal">
+                            <i class="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i>
+                        </a>
+                    </td>
+                </tr>
+            `;
+            dataTable.append(row);
+        });
+    },
+});
 });
